0f88e8fa07b6b538e47dfbb89c19877540fd1044
Troels-dat21B <troe1912@stud.kea.dk>
2024-02-12 17:21:30 +0100

    Added code for the server task in 06._Server

--- 01_Assignment/main.py
+++ 01_Assignment/main.py
@@ -1,13 +1,16 @@
 import os
 
 def main():
-    
-    folder = os.listdir("../01_Assignment/File_Formats")
-    for file in folder:
-        print(file)
-        with open(f"../01_Assignment/File_Formats/{file}", mode='r', encoding="utf-8") as file:
-            print(file.read())
-            print("\n") #Here for extra space in terminal for readability
+    read_files()
+
 
 if __name__ == "__main__":
     main()
+
+def read_files():
+        folder = os.listdir("../01_Assignment/File_Formats")
+        for file in folder:
+            print(file)
+            with open(f"../01_Assignment/File_Formats/{file}", mode='r', encoding="utf-8") as file:
+                print(file.read())
+                print("\n") #Here for extra space in terminal for readability
--- 06._Servers/node/app.js
+++ 06._Servers/node/app.js
@@ -1,4 +1,6 @@
 import express from "express";
+import fs from "fs";
+
 
 const app = express(); //Server instans
 
@@ -6,17 +8,69 @@ const app = express(); //Server instans
     res.send("Hello World");
 }); */ //Dette er standarden for get requests
 
+const folder = fs.readdirSync("../../01_Assignment/File_Formats"); //Finder filerne i mappen
+
 app.get("/", (req, res) => {
-    res.send({message: "Hello World"}); //java script object
+    res.send({
+        message: "skriv følgende efter '/' for at få vist de andre routes:"
+            + "txtRoute, "
+            + "yamlRoute, "
+            + "jsonRoute, "
+            + "csvRoute, "
+            + "docRoute, "
+            + "xmlRoute."
+    }); //java script object
     
 });
 
-app.get("/otherRoute", (req, res) => {
-    res.send({message: "Other Route"});
+//txt fil
+app.get("/txtRoute", (req, res) => {
+    const string =  `../../01_Assignment/File_Formats/${folder[0]}`; //Finder filen i mappen
+    const fileContent = fs.readFileSync(string, "utf-8");
+    res.send(fileContent);
 });
 
-app.post("/postRoute", (req, res) => {  //Post request
-    res.send({message: "Post Route"});
+//yaml fil
+app.get("/yamlRoute", (req, res) => {
+    const string =  `../../01_Assignment/File_Formats/${folder[1]}`; //Finder filen i mappen
+    const fileContent = fs.readFileSync(string, "utf-8");
+    res.send(fileContent);
 });
+
+//json fil
+app.get("/jsonRoute", (req, res) => {
+    const string =  `../../01_Assignment/File_Formats/${folder[2]}`; //Finder filen i mappen
+    const fileContent = fs.readFileSync(string, "utf-8");
+    res.send(fileContent);
+});
+
+//csv fil
+app.get("/csvRoute", (req, res) => {
+    const string =  `../../01_Assignment/File_Formats/${folder[3]}`; //Finder filen i mappen
+    const fileContent = fs.readFileSync(string, "utf-8");
+    res.send(fileContent);
+});
+
+//doc fil
+app.get("/docRoute", (req, res) => {
+    const string =  `../../01_Assignment/File_Formats/${folder[4]}`; //Finder filen i mappen
+    const fileContent = fs.readFileSync(string, "utf-8");
+    res.send(fileContent);
+});
+
+//xml fil
+app.get("/xmlRoute", (req, res) => {
+    const string =  `../../01_Assignment/File_Formats/${folder[5]}`; //Finder filen i mappen
+    console.log(fs.readFileSync(string, "utf-8").toString());
+    const fileContent = fs.readFileSync(string, "utf-8").toString();
+    res.send(fileContent);
+});
+
+
+/* app.post("/postRoute", (req, res) => {  //Post request
+    res.send({message: "Post Route"});
+}); */
+
+
 const PORT = 8080; //Porten som serveren skal køre på
 app.listen(PORT, () => console.log("Server is running on port:", PORT)); //Starter appen på port 8080
