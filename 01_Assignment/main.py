449da941df85b3e109da99fce72b51ab079044e5
Troels-dat21B <troe1912@dat.stud.kea>
2024-02-20 10:30:09 +0100

    Added changes for assignment 01

--- 01_Assignment/main.py
+++ 01_Assignment/main.py
@@ -1,11 +1,15 @@
 import os
+import csv
+import json
+import xml.etree.ElementTree as xml
+import yaml
+#import pandas as pd
 
 def main():
-    read_files()
+    read_xml()
 
+folder = os.listdir("../01_Assignment/File_Formats")
 
-if __name__ == "__main__":
-    main()
 
 def read_files():
         folder = os.listdir("../01_Assignment/File_Formats")
@@ -14,3 +18,28 @@ def read_files():
             with open(f"../01_Assignment/File_Formats/{file}", mode='r', encoding="utf-8") as file:
                 print(file.read())
                 print("\n") #Here for extra space in terminal for readability
+                
+def read_csv():
+    with open("../01_Assignment/File_Formats/"+folder[3], mode='r') as file:
+        csv_reader = csv.reader(file)
+        for row in csv_reader:
+            print(row)
+
+def read_json():
+    with open("../01_Assignment/File_Formats/"+folder[2], mode='r') as file:
+        json_reader = json.load(file)
+        print(json_reader)
+
+def read_xml():
+    with open("../01_Assignment/File_Formats/"+folder[4], mode='r') as file:
+        xml_reader = xml.parse(file)
+        root = xml_reader.getroot()
+        print(root.text)
+
+def read_yaml():
+    with open("../01_Assignment/File_Formats/"+folder[1], mode='r') as file:
+        yaml_reader = yaml.load(file)
+        print(yaml_reader)
+
+if __name__ == "__main__":
+    main()
